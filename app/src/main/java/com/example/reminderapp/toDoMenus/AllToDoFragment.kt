1092b39a992cfff20588415c8010e072ad9b376f
PauPau-Cor <PauPauCoronaP@gmail.com>
2023-01-17 12:18:17 -0600

    Second version of AllToDoFragment layout, added date edit text and refined buttongroup -Laptop

--- app/src/main/java/com/example/reminderapp/toDoMenus/AllToDoFragment.kt
+++ app/src/main/java/com/example/reminderapp/toDoMenus/AllToDoFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.reminderapp.R
 import com.example.reminderapp.databinding.FragmentAllToDoBinding
 
 
@@ -18,6 +20,8 @@ class AllToDoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.PriorityGroup.setOnSelectListener {
+            Toast.makeText(context, it.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
